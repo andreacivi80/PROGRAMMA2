216b099cf131fb2272c83f86835cc0004ca97ee2
Andrea Cividini <utente38@iradom.local>
2026-07-31 12:05:59 +0200

    Release English Coach 3.4 mastery insights

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "english-coach-v33";
+const CACHE = "english-coach-v34";
 const BASE = "/PROGRAMMA2/";
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.add(BASE)));
