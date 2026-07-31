9f4a816773c3bd4565e5e4d3a9f627f292ef218d
Andrea Cividini <utente38@iradom.local>
2026-07-31 22:36:59 +0200

    Chiarisce esercizio A-B-C nella versione 2.17

--- public/gambe-leggere/sw.js
+++ public/gambe-leggere/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gambe-leggere-v216";
+const CACHE = "gambe-leggere-v217";
 const BASE = "/PROGRAMMA2/gambe-leggere/";
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.add(BASE)));
