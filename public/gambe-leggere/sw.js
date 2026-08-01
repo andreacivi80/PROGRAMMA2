1f03932dc72fba0cafe7191637b6470a39455570
Andrea Cividini <utente38@iradom.local>
2026-08-01 18:50:44 +0200

    Pubblica Gambe Leggere 2.31

--- public/gambe-leggere/sw.js
+++ public/gambe-leggere/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gambe-leggere-v230";
+const CACHE = "gambe-leggere-v231";
 const BASE = "/PROGRAMMA2/gambe-leggere/";
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.add(BASE)));
