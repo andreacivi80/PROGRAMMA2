4575ef2435beb40f3f194bdbf527289e3d9e26e6
Andrea Cividini <utente38@iradom.local>
2026-08-01 10:51:21 +0200

    Pubblica allenamento guidato versione 2.22

--- public/gambe-leggere/sw.js
+++ public/gambe-leggere/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gambe-leggere-v221";
+const CACHE = "gambe-leggere-v222";
 const BASE = "/PROGRAMMA2/gambe-leggere/";
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.add(BASE)));
