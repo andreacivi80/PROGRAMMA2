719ec3c1bfcd29a824350349adcedd15b4fd6916
Andrea Cividini <utente38@iradom.local>
2026-08-02 07:08:22 +0200

    Pubblica esercizi gambe v2.35

--- public/gambe-leggere/sw.js
+++ public/gambe-leggere/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gambe-leggere-v234";
+const CACHE = "gambe-leggere-v235";
 const BASE = "/PROGRAMMA2/gambe-leggere/";
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.add(BASE)));
